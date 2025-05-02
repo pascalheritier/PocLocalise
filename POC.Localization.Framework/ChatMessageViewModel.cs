cf86728aae626896824aa6dad921f5d7700c5bf8
Pascal Héritier <pascal.heritier@adonite.com>
2025-05-02 19:44:42 +0200

    Add chat message view model

---
+++ POC.Localization.Framework/ChatMessageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace POC.Localization.Framework;
+public class ChatMessageViewModel
+{
+    public ChatMessageViewModel(string _strTranslationKey)
+    {
+        TranslationKey = _strTranslationKey;
+    }
+
+    public string TranslationKey { get; }
+}
